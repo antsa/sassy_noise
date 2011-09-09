7bc2c5d49daed3ea77b9231fe34ecabd3963a7d5
Antti Salonen <salosen.antti@gmail.com>
2011-09-09 10:51:45 +0300

    Correctly registers the extension

--- lib/sassy_noise.rb
+++ lib/sassy_noise.rb
@@ -4,4 +4,4 @@ require "base64"
 require File.join(File.dirname(__FILE__), 'sassy_noise', 'sass_extensions')
 
 extension_path = File.expand_path(File.join(File.dirname(__FILE__), ".."))
-Compass::Frameworks.register('my_extension', :path => extension_path)
+Compass::Frameworks.register('sassy_noise', :path => extension_path)
